2bb22e24240a0fc2fdaee28e7f947a5fcf2fbdd3
prosperouspi <115018951+prosperouspi@users.noreply.github.com>
2024-11-26 10:02:07 -0500

    Setting up connection for other computers

--- client.py
+++ client.py
@@ -1,10 +1,19 @@
 from socket import *
 
+serverName = input('If you are hosting the server on your computer, enter "localhost".\n'
+                   'Otherwise, enter the IP given by the host.\n'
+                   'Type here: ')
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+clientSocket.send(serverName.encode())
+if serverName == 'localhost':
+    serverIP = clientSocket.recv(1024)
+    print(serverIP.decode())
 while True:
-    serverName = 'localhost'
-    serverPort = 12000
+
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName,serverPort))
+    clientSocket.connect((serverName, serverPort))
     sentence = input('Input lowercase sentence:')
     clientSocket.send(sentence.encode())
     modifiedSentence = clientSocket.recv(1024)
--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from socket import *
+
 serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
+hostname = gethostname()
+serverIP = gethostbyname(hostname)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("", serverPort))
 serverSocket.listen(1)
-print('The server is ready to receive')
+print("The server is ready to receive")
+connectionSocket, addr = serverSocket.accept()
+print(addr)
+host = connectionSocket.recv(1024).decode()
+if host == 'localhost':
+    print("localhost connected")
+    connectionSocket.send(serverIP.encode())
+else:
+    print("Another computer has connected")
 while True:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024).decode()
