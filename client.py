d2d6df895828bbca7a49979ad66cb1d194c3f06e
HunterMcGraw <huntercmcgraw@gmail.com>
2024-11-27 20:59:58 -0500

    Updating

--- client.py
+++ client.py
@@ -8,19 +8,17 @@ serverName = input('If you are hosting the server on your computer, enter "local
                    'Type here: ')
 
 def get_board(clientSocket, lock_input):
-    with lock_input:
+    while True:
         with lock_input:
             message = input("\nEnter index (0-8): ")
             clientSocket.send(message.encode())
 
             if message == 'q':
-                quit()
-        try:
+                clientSocket.close()
+
             message = clientSocket.recv(1024)
             if message:
                 print(message.decode())
-        except:
-            print("Disconnected from server.")
 
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -30,5 +28,3 @@ if serverName == 'localhost':
     serverIP = clientSocket.recv(1024)
     print(serverIP.decode())
 threading.Thread(target=get_board, args=(clientSocket, lock_input), daemon=True).start()
-
-clientSocket.close()
