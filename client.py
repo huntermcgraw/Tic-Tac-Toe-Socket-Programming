8d54b7a0dc3bd528fed7d4d1446b183e337b52ab
HunterMcGraw <huntercmcgraw@gmail.com>
2024-11-27 21:49:56 -0500

    Updating

--- client.py
+++ client.py
@@ -9,16 +9,15 @@ serverName = input('If you are hosting the server on your computer, enter "local
 
 def get_board(clientSocket, lock_input):
     while True:
-        with lock_input:
-            message = input("\nEnter index (0-8): ")
-            clientSocket.send(message.encode())
+        message = input("\nEnter index (0-8): ")
+        clientSocket.send(message.encode())
 
-            if message == 'q':
-                clientSocket.close()
+        if message == 'q':
+            clientSocket.close()
 
-            message = clientSocket.recv(1024)
-            if message:
-                print(message.decode())
+        message = clientSocket.recv(1024)
+        if message:
+            print(message.decode())
 
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -27,4 +26,7 @@ clientSocket.send(serverName.encode())
 if serverName == 'localhost':
     serverIP = clientSocket.recv(1024)
     print(serverIP.decode())
-threading.Thread(target=get_board, args=(clientSocket, lock_input), daemon=True).start()
+else:
+    message = clientSocket.recv(1024)
+    print(message.decode())
+get_board(clientSocket, lock_input)
